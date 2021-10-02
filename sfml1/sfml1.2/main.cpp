2651fede1d0e3a32e04560abaf2b2d62db2e1cc1
m3tro1d <qazwf734@gmail.com>
2021-10-02 11:04:00 +0300

    sfml1: rename directory, add new tasks

---
+++ sfml1/sfml1.2/main.cpp
@@ -0,0 +1,21 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+
+int main()
+{
+  sf::RenderWindow window(
+    sf::VideoMode({ 800, 600 }),
+    "Initials",
+    sf::Style::Default,
+    sf::ContextSettings(0, 0, 8)
+  );
+
+  window.clear();
+
+  // TODO
+
+  window.display();
+
+  sf::sleep(sf::seconds(5));
+}
