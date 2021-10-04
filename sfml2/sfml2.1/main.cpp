b2d5d14a589ccdd803e5085667b543802c8be4e5
m3tro1d <qazwf734@gmail.com>
2021-10-04 22:15:34 +0300

    sfml2: implement sfml2.1 task

--- sfml2/sfml2.1/main.cpp
+++ sfml2/sfml2.1/main.cpp
@@ -1,3 +1,11 @@
+/*
+ * Using iterative calculation, we need to get from analytic equation:
+ * y = A * sin(w * t)
+ * to the finite differences form. Let us find the differential:
+ * dy = A * w * cos(w * t)dt.
+ * Then, just replace all differentials with finite differences (deltas), and we're good to go.
+ */
+
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
@@ -15,10 +23,15 @@ int main()
         "Wave Moving Ball");
     sf::Clock clock;
 
-    const sf::Vector2f position = { 10, 350 };
-
     sf::CircleShape ball(BALL_SIZE);
+    ball.setPosition({ 10, 350 });
     ball.setFillColor(sf::Color(0xFF, 0xFF, 0xFF));
+
+    float time = 0;
+    float speedX = 200.f;
+    constexpr float amplitudeY = 80.f;
+    constexpr float periodY = 2;
+    constexpr float cycleFrequency = float(2 * M_PI) / periodY;
 
     while (window.isOpen())
     {
@@ -31,17 +44,28 @@ int main()
             }
         }
 
-        constexpr float speedX = 100.f;
-        constexpr float amplitudeY = 80.f;
-        constexpr float periodY = 2;
+        // Get time difference, sum with the total time
+        const float dt = clock.restart().asSeconds();
+        time += dt;
 
-        const float time = clock.getElapsedTime().asSeconds();
-        const float wavePhase = time * float(2 * M_PI);
-        const float x = speedX * time;
-        const float y = amplitudeY * std::sin(wavePhase / periodY);
-        const sf::Vector2f offset = { x, y };
+        // Calculate finite differences (deltas)
+        sf::Vector2f position = ball.getPosition();
+        const float dx = speedX * dt;
+        const float dy = amplitudeY * cycleFrequency * std::cos(cycleFrequency * time) * dt;
+        const sf::Vector2f offset = { dx, dy };
+        position += offset;
 
-        ball.setPosition(position + offset);
+        // Handle bumping into the walls
+        if ((position.x + 2 * BALL_SIZE >= WINDOW_WIDTH) && (speedX > 0))
+        {
+            speedX = -speedX;
+        }
+        if ((position.x < 0) && (speedX < 0))
+        {
+            speedX = -speedX;
+        }
+
+        ball.setPosition(position);
 
         window.clear();
         window.draw(ball);
