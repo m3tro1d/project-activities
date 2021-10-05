ad4a39146aa3e9e95cca9259adea861ce556162e
m3tro1d <qazwf734@gmail.com>
2021-10-05 17:49:58 +0300

    sfml2: add 03 example and first part of sfml2.2

---
+++ sfml2/sfml2.2/main.cpp
@@ -0,0 +1,48 @@
+﻿#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <cmath>
+
+int main()
+{
+    constexpr int pointCount = 200;
+
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
+    sf::RenderWindow window(
+        sf::VideoMode({ 800, 600 }), "Ellipse",
+        sf::Style::Default, settings);
+
+    sf::ConvexShape shape;
+    shape.setPosition({ 400, 320 });
+    shape.setFillColor(sf::Color(0xFF, 0x09, 0x80));
+
+    shape.setPointCount(pointCount);
+    for (int pointNo = 0; pointNo < pointCount; ++pointNo)
+    {
+        float angle = float(2 * M_PI * pointNo) / float(pointCount);
+        float radius = 200 * std::sin(6 * angle);
+        sf::Vector2f point = sf::Vector2f{
+            radius * std::sin(angle),
+            radius * std::cos(angle),
+        };
+        shape.setPoint(pointNo, point);
+    }
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        // TODO: move the shape on the circle
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+}
